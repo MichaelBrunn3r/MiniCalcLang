4317791ed92ede9cd685d0a2770b9268bdbbbabb
Michael Brunner <MichaelBrunn3r@gmail.com>
2018-06-07 14:52:39 +0200

    Moved code from 'MiniCalcToAST.kt' to 'MiniCalcParser.kt'

--- src/main/kotlin/io/github/orangeutan/minicalc/MiniCalcParser.kt
+++ src/main/kotlin/io/github/orangeutan/minicalc/MiniCalcParser.kt
@@ -2,6 +2,8 @@ package io.github.orangeutan.minicalc
 
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.ParserRuleContext
+import org.antlr.v4.runtime.Token
 import java.net.URL
 
 class MiniCalcParser {
@@ -21,5 +23,101 @@ class MiniCalcParser {
         fun parseResource(resName: String, savePos: Boolean = true): MiniCalcFile {
             return parseResource(MiniCalcParser::class.java.classLoader.getResource(resName), savePos)
         }
+    }
+}
+
+fun Token.startPoint() = Point(this.line, this.charPositionInLine)
+
+fun Token.endPoint() = Point(this.line, this.charPositionInLine)
+
+fun ParserRuleContext.toPosition(savePos: Boolean): Position? {
+    return if (savePos) Position(this.start.startPoint(), this.stop.endPoint())
+           else null
+}
+
+fun MiniCalcAntlrParser.MiniCalcFileContext.toAST(savePos: Boolean = false): MiniCalcFile
+        = MiniCalcFile(
+        this.line().map({ it.statement().toAST(savePos) }),
+        this.toPosition(savePos))
+
+fun MiniCalcAntlrParser.StatementContext.toAST(savePos: Boolean = false): Statement {
+    when(this) {
+        is MiniCalcAntlrParser.VarDeclarationStatementContext ->
+            return VarDeclaration(
+                    this.varDeclaration().assignment().ID().text,
+                    this.varDeclaration().assignment().expression().toAST(savePos),
+                    this.toPosition(savePos))
+        is MiniCalcAntlrParser.AssignmentStatementContext ->
+            return Assignment(ReferenceByName(
+                    this.assignment().ID().text),
+                    this.assignment().expression().toAST(savePos),
+                    this.toPosition(savePos))
+        is MiniCalcAntlrParser.PrintStatementContext ->
+            return Print(
+                    this.print().expression().toAST(savePos),
+                    this.toPosition(savePos))
+        is MiniCalcAntlrParser.InputDeclarationStatementContext ->
+            return InputDeclaration(
+                    this.inputDeclaration().ID().text,
+                    this.inputDeclaration().type().toAST(savePos),
+                    this.toPosition(savePos))
+        else -> throw UnsupportedOperationException(this.javaClass.canonicalName)
+    }
+}
+
+fun MiniCalcAntlrParser.ExpressionContext.toAST(savePos: Boolean = false): Expression {
+    when(this) {
+        is MiniCalcAntlrParser.BinOpContext -> return this.toAST(savePos)
+        is MiniCalcAntlrParser.IntLitContext -> return IntLit(this.text, this.toPosition(savePos))
+        is MiniCalcAntlrParser.DecLitContext -> return DecLit(this.text, this.toPosition(savePos))
+        is MiniCalcAntlrParser.StrLitContext ->
+            return StrLit(this.parts.map({ it.toAST(savePos) }),
+                    this.toPosition(savePos))
+        is MiniCalcAntlrParser.ParenExprContext -> return this.expression().toAST(savePos)
+        is MiniCalcAntlrParser.IDRefContext -> return IDRef(ReferenceByName(this.text),
+                this.toPosition(savePos))
+        is MiniCalcAntlrParser.TypeConversionContext ->
+            return TypeConversion(this.expression().toAST(savePos),
+                    this.targetType.toAST(savePos),
+                    this.toPosition(savePos))
+        else -> throw UnsupportedOperationException(this.javaClass.canonicalName)
+    }
+}
+
+fun MiniCalcAntlrParser.StringLiteralContentContext.toAST(savePos: Boolean = false): StrLitContent {
+    when(this) {
+        is MiniCalcAntlrParser.StrLitConstContentContext -> return StrLitConstContent(this.STR_CONTENT().text,
+                this.toPosition(savePos))
+
+        is MiniCalcAntlrParser.StrLitInterpContentContext ->
+            return StrLitInterpContent(this.expression().toAST(savePos),
+                    this.toPosition(savePos))
+        else -> throw UnsupportedOperationException(this.javaClass.canonicalName)
+    }
+}
+
+fun MiniCalcAntlrParser.TypeContext.toAST(savePos: Boolean = false): Type {
+    when(this) {
+        is MiniCalcAntlrParser.IntTypeContext -> return IntType(this.toPosition(savePos))
+        is MiniCalcAntlrParser.DecTypeContext -> return DecType(this.toPosition(savePos))
+        else -> throw UnsupportedOperationException(this.javaClass.canonicalName)
+    }
+}
+
+fun MiniCalcAntlrParser.BinOpContext.toAST(savePos: Boolean = false): Expression {
+    when(this.operator.text) {
+        "+" -> return AdditionExpr(this.left.toAST(savePos),
+                this.right.toAST(savePos),
+                this.toPosition(savePos))
+        "-" -> return SubtractionExpr(this.left.toAST(savePos),
+                this.right.toAST(savePos),
+                this.toPosition(savePos))
+        "*" -> return MultiplicationExpr(this.left.toAST(savePos),
+                this.right.toAST(savePos),
+                this.toPosition(savePos))
+        "/" -> return DivisionExpr(this.left.toAST(savePos),
+                this.right.toAST(savePos),
+                this.toPosition(savePos))
+        else -> throw UnsupportedOperationException(this.javaClass.canonicalName)
     }
 }
