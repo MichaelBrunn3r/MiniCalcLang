b038915b89b45ccfe5e77fb1634f307d1a2ef3b1
Michael Brunner <MichaelBrunn3r@gmail.com>
2018-06-07 19:51:49 +0200

    Added attribute 'isResolved'

--- src/main/kotlin/io/github/orangeutan/minicalc/Named.kt
+++ src/main/kotlin/io/github/orangeutan/minicalc/Named.kt
@@ -5,12 +5,20 @@ interface Named {
 }
 
 data class ReferenceByName<N: Named>(var name: String, var referred: N? = null) {
+
+    var isResolved = false
+
+    init {
+        if(referred != null) this.isResolved = true
+    }
+
     override fun toString(): String
-        = "Ref($name)[${if(referred == null) "Unsolved" else "Solved"}]"
+        = "Ref($name)[${if(this.isResolved) "Solved" else "Unsolved"}]"
 
     fun tryToResolve(candidates: List<N>): Boolean {
         val res: N? = candidates.find { it.name == this.name }
         this.referred = res
-        return res != null
+        this.isResolved = res != null
+        return this.isResolved
     }
 }
